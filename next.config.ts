d7eb7a032d397c3d8751860913f54aac517b9572
Park GyeongHwan <mythos2495@gmail.com>
2025-05-05 01:08:13 +0900

    배포를위한준비

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
-  // basePath: "/",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: ".",
 };
 
-export default nextConfig;
+module.exports = nextConfig;

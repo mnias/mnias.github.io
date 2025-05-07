34df90676a75b798b91210f457a888ab109deb34
Park GyeongHwan <mythos2495@gmail.com>
2025-05-07 17:26:24 +0900

    내용 수정

--- src/app/AdminChatbotInfo.tsx
+++ src/app/AdminChatbotInfo.tsx
@@ -5,12 +5,16 @@ export default function AdminChatbotInfo() {
       <div className="mb-8">
         AI와 대화하여 관리자 기능 설명을 확인할 수 있는 페이지
       </div>
-      <ul>
-        <li>- 백엔드 API없이 nextjs API Routes와 openai 라이브러리를 사용</li>
-      </ul>
       <div className="my-8">
         <strong>참여인원</strong> - 프론트엔드 1명, 기획&디자이너 1명
       </div>
+
+      <section className="space-y-8">
+        <h3 className="mt-8 font-bold text-lg mb-4">주요 개발 내용</h3>
+        <ul className="space-y-2">
+          <li>- 백엔드 API없이 nextjs API Routes와 openai 라이브러리를 사용</li>
+        </ul>
+      </section>
       <ul>
         <li className="mt-12 text-18 font-bold">주요 기술 스택</li>
         <li>- Next.js</li>
--- src/app/AdminInfo.tsx
+++ src/app/AdminInfo.tsx
@@ -14,25 +14,31 @@ export default function AdminInfo() {
         <strong>참여인원</strong> - 프론트엔드 1명, 백엔드 1명, 기획&디자이너
         1명
       </div>
-      <ul>
-        <li>- 차트 라이브러리(nivo)를 활용한 매출 데이터 시각화</li>
-        <li>- React.lazy를 적용한 초기 로딩 시간 단축</li>
-        <li>- 메모이제이션을 활용하여 주문서 페이지 리렌더링 최적화</li>
-        <li>- zod라이브러리를 활용한 데이터 유효성 검증</li>
-        <li>
-          - 하나의 코드로 여러개의 서비스 관리, 배포하는 환경 구축을 통한
-          휴먼에러 감소 및 코드 리뷰속도 단축
-        </li>
-        <li>- aws-cli를 활용한 AWS S3 버킷에 정적배포</li>
-      </ul>
-      <ul>
-        <li className="mt-12 text-18 font-bold">주요 기술 스택</li>
-        <li>- React</li>
-        <li>- Redux toolkit</li>
-        <li>- styled-components</li>
-        <li>- Typescript</li>
-        <li>- yarn</li>
-      </ul>
+      <section className="space-y-8">
+        <h3 className="mt-8 font-bold text-lg mb-4">주요 개발 내용</h3>
+        <ul className="space-y-2">
+          <li>- 차트 라이브러리(nivo)를 활용한 매출 데이터 시각화</li>
+          <li>- React.lazy를 적용한 초기 로딩 시간 단축</li>
+          <li>- 메모이제이션을 활용하여 주문서 페이지 리렌더링 최적화</li>
+          <li>- zod라이브러리를 활용한 데이터 유효성 검증</li>
+          <li>
+            - 하나의 코드로 여러개의 서비스 관리, 배포하는 환경 구축을 통한
+            휴먼에러 감소 및 코드 리뷰속도 단축
+          </li>
+          <li>- aws-cli를 활용한 AWS S3 버킷에 정적배포</li>
+        </ul>
+      </section>
+
+      <section>
+        <h3 className="mt-8 font-bold text-lg mb-4">주요 기술 스택</h3>
+        <ul>
+          <li>- React</li>
+          <li>- Redux toolkit</li>
+          <li>- styled-components</li>
+          <li>- Typescript</li>
+          <li>- yarn</li>
+        </ul>
+      </section>
     </>
   );
 }
